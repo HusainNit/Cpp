9c3a8a1f8b908f4080a0c5030b85176e3f50fa79
HusainNit <vaintime21@gmail.com>
2026-06-07 16:28:49 +0300

    feat(constexper_function): added the constexper function difi

--- foundation/src/func_constexpr.cpp
+++ foundation/src/func_constexpr.cpp
@@ -1,9 +1,30 @@
 #include "func_constexpr.h"
+#include <iostream>
 
 //  constexpr function must evaluate at compile-time, when the constant expression it is part of must evaluate at compile time (Otherwise, a constexpr function may be evaluated at either compile-time (if eligible) or runtime. ) 
 
 // To be eligible for compile-time execution, all arguments must be constant expressions
 
-constexpr int func_constexpre() {
+// constexper must be fully definde for varible call (cant do forward call)
+
+// constexpr functions: 
+	// cant have I\O operation inside it (cout/cin)
+	// chain effect: constexpr function can call other functions, but it must the other also constexpr as will
+	// we can make constexper runtime, by using it in output operation(cout) and have not const arugments
+	// both way of compilation made possible to serve both runtimes, and dont have to create 2 same behave functions
+
+
+constexpr double calccirclecumfer(double redius) {
+	constexpr double pi{ 3.14 };
+
+	return (2.0 * redius * pi);
+}
+
+void func_constexpre() {
+
+	// normal function are not allowed in constant expressions.(we cant intlize varible of constexpr with normal call function)
+
+	constexpr double circ{ calccirclecumfer(5) }; // calccirclecumfer MUST be constexpr , and the arugment is const
+	std::cout << "function of constexper, for circle cumfer: " << circ;
 
 }
--- foundation/src/main.cpp
+++ foundation/src/main.cpp
@@ -1,11 +1,14 @@
 #include "numsSys.h"
 #include "optimization.h" // just explinations
 #include "bits.h"
+#include "func_constexpr.h"
 int main() {
 
 	//numsSys();
 
-	bits();
+	//bits();
+
+	func_constexpre();
 
 	return 0;
 }
