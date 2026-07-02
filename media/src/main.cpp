1e1f3e6bc6a760d16c34dc55781dbd24ade13b75
HusainNit <vaintime21@gmail.com>
2026-07-02 15:34:46 +0300

    add(media): add media part

---
+++ media/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() {
+	/*
+		1. sound: https://it.uw.edu//wp-content/uploads/2013/09/DigitalAudioBasics.pdf , https://ciechanow.ski/sound/ , https://wiki.xiph.org/Videos/Digital_Show_and_Tell
+		2. video: https://scratchapixel.com/ , https://workflow.frame.io/guide/ , https://github.com/leandromoreira/digital_video_introduction 
+	*/
+
+	return 0;
+}
