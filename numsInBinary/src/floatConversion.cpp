00aae385c4cc141624aa10a85da44d98f758b93b
HusainNit <vaintime21@gmail.com>
2026-07-09 14:16:10 +0300

    feat(float-init): setup first init understanding of the float

--- numsInBinary/src/floatConversion.cpp
+++ numsInBinary/src/floatConversion.cpp
@@ -7,4 +7,22 @@ void floatConverison() {
 		for praticing float: https://www.h-schmidt.net/
 	*/
 
+
+	/*
+		decimal positional (place-value):
+			22.57 = 2 * (10^1) + 2*(10^0) + 5 * (10^-1) + 7 * (10^-2)
+
+		decimal positional (place-value): use | for sperate only
+			101.1001 = 1 * (2^2) + 0 + 1 * (2^0) | + 1 * (2^-1) + 0 + 0 + 1 * (2^-4)
+	*/
+
+
+	/*
+		representation:
+			32bits = 4bytes
+			single precision floating point: [Sign = 1bit] + [Exponent = 8bits] + [Mantissa = 23bits]
+	
+	*/
+
+
 }
